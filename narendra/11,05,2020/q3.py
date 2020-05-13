528f79327e5c0825e7638f843c04c0b7798285e9
imnaren <65221462+imnaren@users.noreply.github.com>
2020-05-13 15:19:58 +0530

    11,05,2020

---
+++ narendra/11,05,2020/q3.py
@@ -0,0 +1,16 @@
+
+import math
+
+
+def solve(A):
+    k = math.sqrt(2*A)
+    if(k==0):
+        return 0
+    if(A-k*(k+1)/2>=0):
+        return k
+    else:
+        return k-1
+    
+
+A=2
+print(math.ceil(solve(A)))
